140e6e1e8a55597acb7677320f8bdad98d0a74a6
Matheus Oliveira <matheus.vetor@gmail.com>
2021-12-17 16:46:00 -0300

    Add missing test to cover when the sum is 10 to returns 0

--- spec/barcode_checker_spec.rb
+++ spec/barcode_checker_spec.rb
@@ -7,5 +7,6 @@ RSpec.describe BarcodeChecker do
     it { expect { described_class.calculate_check_digit(9780143007234) }.to raise_error('The passing param needs to be a string') }
     it { expect(described_class.calculate_check_digit('978014300723')).to eq(4) }
     it { expect(described_class.calculate_check_digit('978123456789')).to eq(7) }
+    it { expect(described_class.calculate_check_digit('978316148410')).to eq(0) }
   end
 end
